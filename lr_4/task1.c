a2a63639115ffb35e9170a961998d9381f80a76e
Artur Sarakhman <artur.sarahman@gmail.com>
2021-02-21 00:55:19 +0200

    task1 completed

--- lr_4/task1.c
+++ lr_4/task1.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int custom_rand(int a, int b){
+    int ret_val = 0;
+    for(int i = a; i<b; ++i){
+        if(rand() % i == 0)
+            ret_val = i;
+    }
+    return ret_val;
+}
+
+int main(void){
+    int val,
+        sum = 0,
+        lower_bound = 1,
+        upper_bound = 100;
+    printf("Randomize 100 numbers in the range %d to %d:\n", lower_bound, upper_bound);
+    for(int i = 0; i < 100; ++i){
+        printf("%d ", val = custom_rand(lower_bound, upper_bound));
+        sum += val;
+    }
+    printf("\nAverage value after 100 attempts = %f", (double)sum/100);
+	return 0;
+}
